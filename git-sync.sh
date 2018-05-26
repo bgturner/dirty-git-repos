70a142e6ba231ca6ba8a9c8211f63a163a471c1f
Benjamin Turner <benjamin@passionsplay.com>
2018-05-26 15:49:27 -0700

    Add paths to exclude searching

--- git-sync.sh
+++ git-sync.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 find ~ -type d \
+	-not \( -path '*/.vim*/*' -prune \) \
+	-not \( -path '*/.emacs.d/*' -prune \) \
+	-not \( -path '*/.fzf/*' -prune \) \
+	-not \( -path '*/.cargo/*' -prune \) \
+	-not \( -path '*/.oh-my-zsh/*' -prune \) \
+	-not \( -path '*/.nvm/*' -prune \) \
+	-not \( -path '*/.Trash/*' -prune \) \
+	-not \( -path '*/Library/*' -prune \) \
+	-not \( -path '*/Downloads/*' -prune \) \
 	-name "*.git" \
 	| sed 's:.git$::g'
